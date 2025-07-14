c80aa3fc096244c9751d6d3eebf2cbab9e0a3e8a
Ian Ramos <dev1an@protonmail.com>
2025-07-13 23:41:24 -0300

    Updated home page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+	<title>Home - Svelte Store Demo</title>
+</svelte:head>
 <div class="flex items-center justify-center py-20">
 	<h1 class="text-4xl font-bold text-foreground font-integral">Hello World</h1>
 </div>
